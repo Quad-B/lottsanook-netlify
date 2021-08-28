61cac207bf2c4c34a1b519bd637e9f5a90c44ce4
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-08-28 13:55:09 +0700

    JSON.stringify(data)

--- express/server.js
+++ express/server.js
@@ -56,7 +56,7 @@ router.get('/index2', (req, res) => {
     }
     //res.send(data);
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(data);
+    res.write(JSON.stringify(data));
     res.end();
   } else {
     fetch('https://news.sanook.com/lotto/check/' + req.query.date + '/', { redirect: 'error' })
@@ -130,7 +130,7 @@ router.get('/index2', (req, res) => {
             }
             //res.send(data)
             res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.write(data);
+            res.write(JSON.stringify(data));
             res.end();
           });
         } else {
@@ -154,7 +154,7 @@ router.get('/index2', (req, res) => {
           }
           //res.send(data)
           res.writeHead(200, { 'Content-Type': 'application/json' });
-          res.write(data);
+          res.write(JSON.stringify(data));
           res.end();
         }
       })
@@ -162,7 +162,7 @@ router.get('/index2', (req, res) => {
         let data = [["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e481", 0], ["\u0e40\u0e25\u0e02\u0e2b\u0e19\u0e49\u0e323\u0e15\u0e31\u0e27", 0, 0], ["\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e223\u0e15\u0e31\u0e27", 0, 0], ["\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e222\u0e15\u0e31\u0e27", 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e02\u0e49\u0e32\u0e07\u0e40\u0e04\u0e35\u0e22\u0e07\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e481", 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e482", 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e483", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e484", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e485", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
         //res.send(data)
         res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(data);
+        res.write(JSON.stringify(data));
         res.end();
         console.log(err)
       });
