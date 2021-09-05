9bfeb29a09cb96d08994df7245cec01154cba23c
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-09-06 01:31:22 +0700

    test

--- express/server.js
+++ express/server.js
@@ -78,104 +78,94 @@ router.get('/', (req, res) => {
       res.write(JSON.stringify(data));
       res.end();
     } else {
-      try {
-        fetch('https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%87%E0%B8%A7%E0%B8%94-' + req.query.date.substring(0, 2) + '-' + encodeURI(monthtext) + '-' + req.query.date.substring(4, 8) + '.aspx', { redirect: 'error' })
-          .then(res => res.text())
-          .then((body) => {
-            data = [["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e481", 0], ["\u0e40\u0e25\u0e02\u0e2b\u0e19\u0e49\u0e323\u0e15\u0e31\u0e27", 0, 0], ["\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e223\u0e15\u0e31\u0e27", 0, 0], ["\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e222\u0e15\u0e31\u0e27", 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e02\u0e49\u0e32\u0e07\u0e40\u0e04\u0e35\u0e22\u0e07\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e481", 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e482", 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e483", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e484", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e485", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-            let $ = cheerio.load(body)
+      fetch('https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%87%E0%B8%A7%E0%B8%94-' + req.query.date.substring(0, 2) + '-' + encodeURI(monthtext) + '-' + req.query.date.substring(4, 8) + '.aspx', { redirect: 'error' })
+        .then(res => res.text())
+        .then((body) => {
+          data = [["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e481", 0], ["\u0e40\u0e25\u0e02\u0e2b\u0e19\u0e49\u0e323\u0e15\u0e31\u0e27", 0, 0], ["\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e223\u0e15\u0e31\u0e27", 0, 0], ["\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e222\u0e15\u0e31\u0e27", 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e02\u0e49\u0e32\u0e07\u0e40\u0e04\u0e35\u0e22\u0e07\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e481", 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e482", 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e483", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e484", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e485", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+          let $ = cheerio.load(body)
 
-            let numberpush = []
+          let numberpush = []
 
-            $('.lot-dc').toArray().forEach(element => {
-              try {
-                //console.log(element.firstChild.data)
-                numberpush.push(element.firstChild.data)
-              } catch (error) {
+          $('.lot-dc').toArray().forEach(element => {
+            try {
+              //console.log(element.firstChild.data)
+              numberpush.push(element.firstChild.data)
+            } catch (error) {
 
-              }
-            });
-
-            if ($('div').toArray()[2] == null) {
-              //res.send(data)
-              res.writeHead(200, { 'Content-Type': 'application/json' });
-              res.write(JSON.stringify(data));
-              res.end();
-              return
             }
+          });
 
-            let threefirst = []
-            let threeend = []
+          if ($('div').toArray()[2] == null) {
+            //res.send(data)
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(data));
+            res.end();
+            return
+          }
 
-            data[0][1] = numberpush[0]
+          let threefirst = []
+          let threeend = []
+
+          data[0][1] = numberpush[0]
+          numberpush.shift()
+          if (numberpush[0].split(" ").length > 2) {
+            threeend = numberpush[0].split(" ")
+            data[2][1] = threeend[0].replace(/\xc2\xa0/, '');
+            data[2][2] = threeend[1].replace(/\xc2\xa0/, '');
+            data[2][3] = threeend[2].replace(/\xc2\xa0/, '');
+            data[2][4] = threeend[3].replace(/\xc2\xa0/, '');
+          } else {
+            threefirst = numberpush[0].split(" ")
+            data[1][1] = threefirst[0].replace(/\xc2\xa0/, '');
+            data[1][2] = threefirst[1].replace(/\xc2\xa0/, '');
+          }
+          numberpush.shift()
+          if (numberpush[0].length == 2) {
+            data[3][1] = numberpush[0]
             numberpush.shift()
-            if (numberpush[0].split(" ").length > 2) {
-              threeend = numberpush[0].split(" ")
-              data[2][1] = threeend[0].replace(/\xc2\xa0/, '');
-              data[2][2] = threeend[1].replace(/\xc2\xa0/, '');
-              data[2][3] = threeend[2].replace(/\xc2\xa0/, '');
-              data[2][4] = threeend[3].replace(/\xc2\xa0/, '');
-            } else {
-              threefirst = numberpush[0].split(" ")
-              data[1][1] = threefirst[0].replace(/\xc2\xa0/, '');
-              data[1][2] = threefirst[1].replace(/\xc2\xa0/, '');
-            }
+          } else {
+            threeend = numberpush[0].split(" ")
+            data[2][1] = threeend[0].replace(/\xc2\xa0/, '');
+            data[2][2] = threeend[1].replace(/\xc2\xa0/, '');
             numberpush.shift()
-            if (numberpush[0].length == 2) {
-              data[3][1] = numberpush[0]
-              numberpush.shift()
-            } else {
-              threeend = numberpush[0].split(" ")
-              data[2][1] = threeend[0].replace(/\xc2\xa0/, '');
-              data[2][2] = threeend[1].replace(/\xc2\xa0/, '');
-              numberpush.shift()
-              data[3][1] = numberpush[0]
-              numberpush.shift()
-            }
-            data[4][1] = padLeadingZeros((data[0][1] - 1), 6);
-            data[4][2] = padLeadingZeros((data[0][1] + 1), 6);
+            data[3][1] = numberpush[0]
+            numberpush.shift()
+          }
+          data[4][1] = padLeadingZeros((data[0][1] - 1), 6);
+          data[4][2] = padLeadingZeros((data[0][1] + 1), 6);
 
-            let wave = 5;
-            let minwave = 0;
-            let maxwave = 5;
+          let wave = 5;
+          let minwave = 0;
+          let maxwave = 5;
 
-            for (const type of numberpush) {
-              if (wave >= 5) {
-                if (minwave < maxwave) {
-                  minwave++;
-                  data[wave][minwave] = type
-                }
-              }
-              if (minwave == maxwave && wave == 5) {
-                minwave = 0;
-                maxwave = 10;
-                wave = 6;
-              }
-              if (minwave == maxwave && wave == 6) {
-                minwave = 0;
-                maxwave = 50;
-                wave = 7;
-              }
-              if (minwave == maxwave && wave == 7) {
-                minwave = 0;
-                maxwave = 100;
-                wave = 8;
+          for (const type of numberpush) {
+            if (wave >= 5) {
+              if (minwave < maxwave) {
+                minwave++;
+                data[wave][minwave] = type
               }
             }
+            if (minwave == maxwave && wave == 5) {
+              minwave = 0;
+              maxwave = 10;
+              wave = 6;
+            }
+            if (minwave == maxwave && wave == 6) {
+              minwave = 0;
+              maxwave = 50;
+              wave = 7;
+            }
+            if (minwave == maxwave && wave == 7) {
+              minwave = 0;
+              maxwave = 100;
+              wave = 8;
+            }
+          }
 
-            if ($('div').toArray()[2].firstChild.data != null && $('div').toArray()[2].firstChild.data != ' เวลา 14:30-16:00น.') {
-              fs.appendFile('/tmp/' + req.query.date + '.txt', JSON.stringify(data), function (err) {
-                if (err) throw err;
-                //console.log('Saved!');
-                if (req.query.from !== undefined) {
-                  data[0][0] = req.query.date.substring(0, 2) + monthtext + req.query.date.substring(4, 8)
-                }
-                //res.send(data)
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.write(JSON.stringify(data));
-                res.end();
-              });
-            } else {
+          if ($('div').toArray()[2].firstChild.data != null && $('div').toArray()[2].firstChild.data != ' เวลา 14:30-16:00น.') {
+            fs.appendFile('/tmp/' + req.query.date + '.txt', JSON.stringify(data), function (err) {
+              if (err) throw err;
+              //console.log('Saved!');
               if (req.query.from !== undefined) {
                 data[0][0] = req.query.date.substring(0, 2) + monthtext + req.query.date.substring(4, 8)
               }
@@ -183,14 +173,23 @@ router.get('/', (req, res) => {
               res.writeHead(200, { 'Content-Type': 'application/json' });
               res.write(JSON.stringify(data));
               res.end();
+            });
+          } else {
+            if (req.query.from !== undefined) {
+              data[0][0] = req.query.date.substring(0, 2) + monthtext + req.query.date.substring(4, 8)
             }
-          });
-      } catch (err) {
-        data = [["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e481", 0], ["\u0e40\u0e25\u0e02\u0e2b\u0e19\u0e49\u0e323\u0e15\u0e31\u0e27", 0, 0], ["\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e223\u0e15\u0e31\u0e27", 0, 0], ["\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e222\u0e15\u0e31\u0e27", 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e02\u0e49\u0e32\u0e07\u0e40\u0e04\u0e35\u0e22\u0e07\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e481", 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e482", 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e483", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e484", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e485", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(JSON.stringify(data));
-        res.end();
-      }
+            //res.send(data)
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(data));
+            res.end();
+          }
+        }).catch(error => {
+          //console.log(error);
+          data = [["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e481", 0], ["\u0e40\u0e25\u0e02\u0e2b\u0e19\u0e49\u0e323\u0e15\u0e31\u0e27", 0, 0], ["\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e223\u0e15\u0e31\u0e27", 0, 0], ["\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e222\u0e15\u0e31\u0e27", 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e02\u0e49\u0e32\u0e07\u0e40\u0e04\u0e35\u0e22\u0e07\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e481", 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e482", 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e483", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e484", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e485", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.write(JSON.stringify(data));
+          res.end();
+        });
     }
   }
 
@@ -372,7 +371,7 @@ router.get('/reto', (req, res) => {
 });
 
 router.get('/god', async (req, res) => {
-  /*let year = 2533;
+  let year = 2533;
   let preyearlist = [];
   let preyearsuperlist = [];
   let yearlist = [];
@@ -469,14 +468,7 @@ router.get('/god', async (req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify(yearlist));
     res.end();
-  });*/
-  await fetch('https://raw.githubusercontent.com/boyphongsakorn/testrepo/main/god')
-    .then(res => res.json())
-    .then((body) => {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.write(JSON.stringify(body));
-      res.end();
-    })
+  });
 });
 
 router.get('/gdpy', (req, res) => {
